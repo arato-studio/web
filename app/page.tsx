79f7451fa569598d22243e43eb8c9e3555e096d9
Axel <axelmychro@gmail.com>
2026-09-18 17:07:14 +0700

    perbesar tombol ajakan

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <section className="flex flex-col items-center justify-center gap-8 max-w-prose">
-        <h2 className="text-2xl text-center text-balance text-shadow-xs tracking-wide text-shadow-primary">
+        <h2 className="text-2xl text-center text-balance text-shadow-xs tracking-wide text-shadow-primary md:text-4xl">
           Bikin{" "}
           <b>
             <i>Brand</i> Kamu
@@ -14,29 +14,29 @@ export default function Home() {
           Tampil Beda Lewat Website & Video <b>Profesional</b>.
         </h2>
 
-        <p className="max-w-md text-balance border-l-muted tracking-wider border-l-4 pl-4 text-shadow-xs text-shadow-primary">
+        <p className="max-w-md text-balance border-l-muted tracking-wider border-l-4 pl-4 text-shadow-xs text-shadow-primary md:text-xl">
           Kami bantu UMKM dan kreator ngembangin kehadiran digital lewat desain
           web modern dan editing video yang memikat.
         </p>
       </section>
 
-      <section className="flex flex-col items-center justify-center gap-8 max-w-prose">
-        <h2 className="text-xl text-center text-balance text-shadow-xs tracking-wide text-shadow-primary">
+      <section className="flex flex-col items-center justify-center gap-8 not-md:max-w-prose">
+        <h2 className="text-xl text-center text-balance text-shadow-xs tracking-wide text-shadow-primary md:text-2xl">
           Mau jasa cepat, atau proyek jangka panjang?
         </h2>
-        <p className="text-balance tracking-wider text-shadow-xs text-shadow-primary">
+        <p className="text-balance tracking-wider text-shadow-xs text-shadow-primary md:text-xl">
           Hubungi kami:
         </p>
-        <ul className="flex gap-2 flex-wrap max-w-xs">
+        <ul className="flex gap-4 flex-wrap not-md:max-w-xs">
           {profile.links.map((link) => {
             return (
               <li
                 key={link.label}
-                className={`${link.style} font-bold flex items-center justify-center rounded-lg flex-1 shadow-xs`}
+                className={`${link.style} font-bold flex items-center justify-center rounded-xs flex-1 shadow-xs`}
               >
                 <Link
                   href={link.to}
-                  className="flex items-center gap-4 p-2 min-w-32 min-h-16 text-inherit tracking-widest"
+                  className="flex items-center gap-4 p-2 min-w-48 justify-center min-h-16 text-inherit tracking-widest text-xl"
                 >
                   <Icon icon={link.icon} className="text-2xl" />
                   {link.label}
