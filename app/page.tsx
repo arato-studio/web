a5cdc7223f044c8ed76d56e7be0f432dd6ec2827
Axel <axelmychro@gmail.com>
2026-09-18 17:23:17 +0700

    gabungkan tombol-tombol ajakan ke dalam bagian beranda

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ import { Icon } from "@iconify/react";
 export default function Home() {
   return (
     <>
-      <section className="flex flex-col items-center justify-center gap-8 max-w-prose">
+      <section
+        id="home"
+        className="flex flex-col items-center justify-center gap-8 max-w-prose"
+      >
         <h2 className="text-2xl text-center text-balance text-shadow-xs tracking-wide text-shadow-primary md:text-4xl">
           Bikin{" "}
           <b>
@@ -14,37 +17,37 @@ export default function Home() {
           Tampil Beda Lewat Website & Video <b>Profesional</b>.
         </h2>
 
-        <p className="max-w-md text-balance border-l-muted tracking-wider border-l-4 pl-4 text-shadow-xs text-shadow-primary md:text-xl">
-          Kami bantu UMKM dan kreator ngembangin kehadiran digital lewat desain
-          web modern dan editing video yang memikat.
+        <p className="max-w-md  opacity-80 text-balance border-l-muted tracking-wider border-l-4 pl-4 text-shadow-xs text-shadow-primary md:text-xl">
+          Kami bantu pelajar, kreator, dan UMKM ngembangin kehadiran digital
+          lewat desain web modern dan editing video yang memikat.
         </p>
-      </section>
 
-      <section className="flex flex-col items-center justify-center gap-8 not-md:max-w-prose">
-        <h2 className="text-xl text-center text-balance text-shadow-xs tracking-wide text-shadow-primary md:text-2xl">
-          Mau jasa cepat, atau proyek jangka panjang?
-        </h2>
-        <p className="text-balance tracking-wider text-shadow-xs text-shadow-primary md:text-xl">
-          Hubungi kami:
-        </p>
-        <ul className="flex gap-4 flex-wrap not-md:max-w-xs">
-          {profile.links.map((link) => {
-            return (
-              <li
-                key={link.label}
-                className={`${link.style} font-bold flex items-center justify-center rounded-xs flex-1 shadow-xs`}
-              >
-                <Link
-                  href={link.to}
-                  className="flex items-center gap-4 p-2 min-w-48 justify-center min-h-16 text-inherit tracking-widest text-xl"
+        <article className="flex flex-col items-center justify-center gap-8 not-md:max-w-prose">
+          <h2 className="text-xl text-center text-balance text-shadow-xs tracking-wide text-shadow-primary md:text-2xl">
+            Mau jasa cepat, atau proyek jangka panjang?
+          </h2>
+          <p className="text-balance tracking-wider text-shadow-xs text-shadow-primary md:text-xl">
+            Hubungi kami:
+          </p>
+          <ul className="flex gap-4 flex-wrap not-md:max-w-xs">
+            {profile.links.map((link) => {
+              return (
+                <li
+                  key={link.label}
+                  className={`${link.style} font-bold flex items-center justify-center rounded-sm flex-1 shadow-xs`}
                 >
-                  <Icon icon={link.icon} className="text-2xl" />
-                  {link.label}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+                  <Link
+                    href={link.to}
+                    className="flex items-center gap-4 p-2 min-w-48 justify-center min-h-16 text-inherit tracking-widest text-xl"
+                  >
+                    <Icon icon={link.icon} className="text-2xl" />
+                    {link.label}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </article>
       </section>
     </>
   );
